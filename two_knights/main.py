346f8a4bdbe5aa41e7c8644e0753ab4dfc448f68
risshhabh <26rkshah@gmail.com>
2023-08-11 15:32:19 -0700

    Two Knights in Python

---
+++ two_knights/main.py
@@ -0,0 +1,14 @@
+# https://cses.fi/problemset/task/1072
+
+n = int(input())
+
+for k in range(1, n + 1):
+    K = k ** 2
+    print((
+          (K - 9) * (k - 4) ** 2 # middle
+        + 4 * (K - 3)            # corners
+        + 8 * (K - 4)            # corner sides
+        + 4 * (k - 4) * (K - 5)  # sides
+        + 4 * (K - 5)            # L2 corners
+        + 4 * (k - 4) * (K - 7)  # L2 sides
+    ) // 2)                      # overcounting XY = YX
